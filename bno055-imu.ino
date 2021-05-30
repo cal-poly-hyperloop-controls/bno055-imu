843d4494fa8d13bc23f234a85872cd6dd3020d5e
Monty <montgomery.okdaily@gmail.com>
2021-05-30 14:41:21 -0700

    display linear accel data from imu onto serial monitor

--- bno055-imu.ino
+++ bno055-imu.ino
@@ -1,8 +1,37 @@
-void setup() {
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
 
+#define BNO055_SAMPLERATE_DELAY_MS 100
+
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("BNO055 IMU");
+
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  delay(1000);
 }
 
 void loop() {
+  imu::Vector<3> linAccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
 
-  
+  Serial.print("X: ");
+  Serial.print(linAccel.x(), 5);
+  Serial.print(" Y: ");
+  Serial.print(linAccel.y());
+  Serial.print(" Z: ");
+  Serial.print(linAccel.z());
+  Serial.println("");
+
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+
 }
